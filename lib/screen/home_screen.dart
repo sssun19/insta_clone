62cc7161b3067567ef349b201e381cfda0d06166
sssun <98_0509@naver.com>
2024-05-21 01:14:18 +0900

    feedArea

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -24,7 +24,8 @@ class StoryArea extends StatelessWidget {
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       child: Row(
-        children: List.generate(10, (index) => UserStory(userName: 'User $index')),
+        children: List.generate(
+            10, (index) => UserStory(userName: 'User $index')),
       ),
     );
   }
@@ -60,8 +61,43 @@ class UserStory extends StatelessWidget {
   }
 }
 
+class FeedData {
+  final String userName;
+  final int likeCount;
+  final String content;
+
+  FeedData(
+      {required this.userName, required this.likeCount, required this.content});
+}
+
+final feedDataList = [
+  FeedData(userName: 'User1', likeCount: 50, content: '오늘 점심은 맛있었다.'),
+  FeedData(userName: 'User2', likeCount: 23, content: '오늘 아침은 맛있었다.'),
+  FeedData(userName: 'User3', likeCount: 13, content: '오늘 저녁은 맛있었다.'),
+  FeedData(userName: 'User4', likeCount: 767, content: '어제 점심은 맛있었다.'),
+  FeedData(userName: 'User5', likeCount: 56, content: '어제 아침은 맛있었다.'),
+  FeedData(userName: 'User6', likeCount: 1, content: '어제 저녁은 맛있었다.'),
+];
+
 class FeedList extends StatelessWidget {
   const FeedList({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemBuilder: (context, index) => FeedItem(feedData: feedDataList[index]),
+      itemCount: feedDataList.length,
+    );
+  }
+}
+
+class FeedItem extends StatelessWidget {
+  final FeedData feedData;
+
+  const FeedItem({required this.feedData, super.key});
 
   @override
   Widget build(BuildContext context) {
