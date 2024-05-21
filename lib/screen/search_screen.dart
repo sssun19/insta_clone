d26d0d0c53349c4bf9c770fe75b71bc98067e715
sssun <98_0509@naver.com>
2024-05-22 01:18:54 +0900

    searchBar

--- lib/screen/search_screen.dart
+++ lib/screen/search_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SearchScreen extends StatelessWidget {
@@ -5,8 +6,56 @@ class SearchScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Icon(Icons.search, size: 100,),
+    return const SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SearchTextBar(),
+            SearchGrid(),
+          ],
+        ),
+      ),
     );
+  }
+}
+
+class SearchTextBar extends StatelessWidget {
+  const SearchTextBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: TextField( //검색 위젯
+          cursorColor: Colors.black,
+          decoration: InputDecoration( //검색어 입력창 데코레이션
+            prefixIcon: const Icon(Icons.search),
+            hintText: '검색',
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Colors.grey.shade200, width: 1),
+            ),
+            enabledBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+              borderSide: BorderSide(color: Colors.grey.shade200, width: 1),
+            ),
+            contentPadding: const EdgeInsets.all(0), //hintText 등 검색창 내 콘텐츠 패딩 0으로 조절 해서 중간에 위치 하도록
+            filled: true,
+            fillColor: Colors.grey.shade200,
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class SearchGrid extends StatelessWidget {
+  const SearchGrid({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
   }
 }
